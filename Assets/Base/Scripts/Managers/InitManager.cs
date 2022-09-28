1c6bfb3cb7bc9d3875d79329b8246d54a1f9e5d7
Cengizhan Doğan <77576073+CengizhanDogan@users.noreply.github.com>
2022-09-28 12:49:43 +0300

    Initial commit

---
+++ Assets/Base/Scripts/Managers/InitManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class InitManager : Singleton<InitManager>
+{
+
+    private void Start()
+    {
+        Invoke("LoadMenuScene", 0.5f);
+    }
+    
+    public void LoadMenuScene()
+    {
+        StartCoroutine(LoadMenuSceneCo());
+    }
+
+    private IEnumerator LoadMenuSceneCo()
+    {
+        yield return SceneManager.LoadSceneAsync("UI", LoadSceneMode.Additive);
+        LevelManager.Instance.LoadLastLevel();
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Base/Scripts/Utilities/Editor/SceneSwitcher/Editor/SceneSwitcher.cs
@@ -0,0 +1,70 @@
+﻿using UnityEditor;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace UnityToolbarExtender.ToolbarButtons
+{
+	static class ToolbarStyles
+	{
+		public static readonly GUIStyle commandButtonStyle;
+
+		static ToolbarStyles()
+		{
+			commandButtonStyle = new GUIStyle("Command")
+			{
+				fontSize = 16,
+				alignment = TextAnchor.MiddleCenter,
+				imagePosition = ImagePosition.ImageAbove,
+				fontStyle = FontStyle.Bold
+			};
+		}
+	}
+
+	[InitializeOnLoad]
+	public class SceneSwitchLeftButton
+	{
+		static SceneSwitchLeftButton()
+		{
+			ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+		}
+
+		static void OnToolbarGUI()
+		{
+			GUILayout.FlexibleSpace();
+
+			if (GUILayout.Button(new GUIContent("1", "Start Scene 1"), ToolbarStyles.commandButtonStyle))
+			{
+				SceneHelper.StartScene(0);
+			}
+
+			if (GUILayout.Button(new GUIContent("2", "Start Scene 2"), ToolbarStyles.commandButtonStyle))
+			{
+				SceneHelper.StartScene(1);
+			}
+
+			if (GUILayout.Button(new GUIContent("3", "Start Scene 2"), ToolbarStyles.commandButtonStyle))
+			{
+				SceneHelper.StartScene(2);
+			}
+
+		}
+	}
+
+	static class SceneHelper
+	{
+		static string sceneToOpen;
+
+		public static void StartScene(int sceneIndex)
+		{
+			if (EditorApplication.isPlaying)
+			{
+				EditorApplication.isPlaying = false;
+			}
+			if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+				EditorApplication.OpenScene(EditorBuildSettings.scenes[sceneIndex].path);
+
+		}
+
+	}
+}
