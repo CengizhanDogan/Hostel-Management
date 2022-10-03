e3614c746acfa38baea891077241e0305b3b105e
Cengizhan Doğan <cengizhan.dgn@gmail.com>
2022-10-03 14:10:09 +0300

    Room buy done

--- Assets/-GameFolder-/Scripts/Door.cs
+++ Assets/-GameFolder-/Scripts/Door.cs
@@ -1,17 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
-public class Door : MonoBehaviour, IInteractable
+public class Door : MonoBehaviour, IInteractable, IExitable
 {
     [SerializeField] private Room room;
+    [SerializeField] private Rigidbody doorRb;
+    [SerializeField] private Transform longWalls;
+
+    private bool exited;
+    private Collider coll;
+
+    private void Start()
+    {
+        coll = GetComponent<Collider>();
+        PlayerPrefs.SetInt(PlayerPrefKeys.Coin, 1000);
+    }
 
     public void Interact(ManagerBehaviour manager)
     {
-        if (manager.GetCustomer() == null || room.GetCustomer() != null) return;
+        exited = false;
 
-        var customer = manager.GetCustomer();
-        customer.SetToRoom(room);
-        room.SetCustomer(customer);
+        if (room.available)
+        {
+            if (manager.GetCustomer() == null || room.GetCustomer() != null) return;
+
+            var customer = manager.GetCustomer();
+            customer.SetToRoom(room);
+            room.SetCustomer(customer);
+        }
+        else
+        {
+            StartCoroutine(SpendMoneyToRoom());
+        }
+    }
+
+    private IEnumerator SpendMoneyToRoom()
+    {
+        while (!exited)
+        {
+            Debug.Log("Spend");
+            if (PlayerPrefs.GetInt(PlayerPrefKeys.Coin) > 0 && room.roomValue > 0)
+            {
+                PlayerPrefs.SetInt(PlayerPrefKeys.Coin, PlayerPrefs.GetInt(PlayerPrefKeys.Coin) - 1);
+                room.roomValue -= 1;
+                foreach (var item in FindObjectOfType<LevelPanel>().inGameCoinTexts)
+                {
+                    item.text = PlayerPrefs.GetInt(PlayerPrefKeys.Coin).ToString();
+                }
+            }
+            if (room.roomValue <= 0)
+            {
+                coll.enabled = false;
+               
+                room.transform.DOMoveY(0, 1f).SetEase(Ease.OutBack).OnComplete(() =>
+                {
+                    coll.enabled = true;
+                    doorRb.isKinematic = false;
+                    room.available = true;
+                });
+                longWalls.transform.DOMoveY(-3, 1f).SetEase(Ease.OutBack);
+            }
+            yield return new WaitForSeconds(0.25f);
+        }
+    }
+    public void Exit()
+    {
+        exited = true;
     }
 }
--- Assets/-GameFolder-/Scripts/Room.cs
+++ Assets/-GameFolder-/Scripts/Room.cs
@@ -6,6 +6,9 @@ public class Room : MonoBehaviour
 {
     private CustomerBehaviour roomCustomer;
 
+    public int roomValue;
+
+    public bool available;
     public void SetCustomer(CustomerBehaviour customer) { roomCustomer = customer; }
     public CustomerBehaviour GetCustomer() { return roomCustomer; }
 }
